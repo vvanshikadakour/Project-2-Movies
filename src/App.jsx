27bb76b4835d5f6e41cd766233d686a381afb8ca
manish chauhan <manishchauhan7783@gmail.com>
2025-12-02 15:10:46 +0530

    first-commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+import "./App.css";
+import Fire from "./assets/fire.png";
+import Star from "./assets/glowing-star.png";
+import Party from "./assets/partying-face.png";
+import Navbar from "./components/Navbar/Navbar";
+import MovieList from "./components/MovieList/MovieList";
+
+const App = () => {
+    return (
+        <div className='app'>
+            <Navbar />
+
+            <MovieList type='popular' title='Popular' emoji={Fire} />
+            <MovieList type='top_rated' title='Top Rated' emoji={Star} />
+            <MovieList type='upcoming' title='Upcoming' emoji={Party} />
+        </div>
+    );
+};
+
+export default App;
